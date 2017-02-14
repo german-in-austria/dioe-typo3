4badd9db4166664737604fd25753f15de19e9864
hcb79 <hcb@hcb.de>
2017-02-14 11:18:58 +0100

    "Nach oben"-Knopf hinzugefügt. Kleinere Anpassungen (Termine, BibTex Druckansicht).

--- fileadmin/js/main.js
+++ fileadmin/js/main.js
@@ -5,10 +5,15 @@
       console.log('Lade Inhalte ab Eintrag: '+$('.article-laden').data("loadbegin"))
       $.post( "", { getlist: "1", begin: $('.article-laden').data("loadbegin") }).done(function( data ) {
         $('.article-laden.lade').replaceWith(data);
+        setTimeout(function(){ $('#top-link-block').affix('checkPosition'); }, 500);
       }).fail(function( data ) {
         console.log(data);
         $('.article-laden.lade').replaceWith('<div>Fehler beim laden!</div>');
       });
-    }
-}).scroll();
+    };
+  }).scroll();
+  if(($(window).height()+150)<$(document).height()) {
+    $('#top-link-block').removeClass('hidden').affix({ offset: {top:150, bottom: 300} });
+    setTimeout(function(){ $('#top-link-block').affix('checkPosition'); }, 500);
+  };
 });})(jQuery);
