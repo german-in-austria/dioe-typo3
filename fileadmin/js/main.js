7ebe538c663243d01d7ebc70f1548e1e3a2c77bc
hcb79 <hcb@hcb.de>
2017-02-15 11:54:48 +0100

    Javascript für mobiles Menü aus footer in main.js ausgelagert.

--- fileadmin/js/main.js
+++ fileadmin/js/main.js
@@ -1,4 +1,31 @@
 (function($){jQuery(document).ready(function($){
+  // DESKTOP
+  if (!("ontouchstart" in document)) {
+    var s = skrollr.init({
+        smoothScrolling: false,
+        forceHeight: true
+    });
+  }
+
+  $('body').on('touchend', '.trigger-mobile-menu', function (e) {
+    e.preventDefault()
+    e.stopPropagation()
+    console.log(e)
+    setTimeout(function(){
+      $('body').toggleClass('show-mobile-nav')
+    }, 250)
+  })
+
+  setInterval(function() {
+      $('.hero').toggleClass('active');
+  }, 8000);
+
+  $(document).on('click', '[data-toggle="lightbox"]', function(event) {
+      event.preventDefault();
+      $(this).ekkoLightbox();
+  });
+
+  // Nachladen
   $(window).scroll(function(event) {
     if($('.article-laden.laden').length==1 && $('.article-laden.laden').offset().top < $(window).scrollTop() + $(window).outerHeight()) {
       $('.article-laden').html('Wird geladen ...').removeClass('laden').addClass('lade');
@@ -12,8 +39,11 @@
       });
     };
   }).scroll();
+
+  // "Nach oben"-Knopf
   if(($(window).height()+150)<$(document).height()) {
     $('#top-link-block').removeClass('hidden').affix({ offset: {top:150, bottom: 300} });
     setTimeout(function(){ $('#top-link-block').affix('checkPosition'); }, 500);
   };
+
 });})(jQuery);
